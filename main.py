d48a8d1bbe642760b56c7f31921e96adb3f671f3
Alvoras <hadrien.bouffier@gmail.com>
2020-11-27 19:43:21 +0100

    Updated

--- main.py
+++ main.py
@@ -11,38 +11,40 @@ import os
 
 def main(screen):
     curses.curs_set(0)
+    screen.nodelay(1)
+    screen.keypad(1)
 
     box_padding = 2
 
     sh, sw = screen.getmaxyx()
     sheet_file = "demo"
-    sheet = Sheet(2, sheet_file, height=sh - box_padding * 4, bpm=480)
+    sheet = Sheet(2, sheet_file, height=sh - box_padding * 4, bpm=240)
 
-    sheet_box = [[1, 1],
+    sheet_box = [[1, 2],
                  [sh - box_padding, sw // 3 - box_padding]]  # [[ul_y, ul_x], [dr_y, dr_x]]
 
     circuit_box = [[1, sw // 3],
-                   [sh - box_padding, sw - box_padding]]  # [[ul_y, ul_x], [dr_y, dr_x]]
+                   [sh - box_padding, sw - 3]]  # [[ul_y, ul_x], [dr_y, dr_x]]
 
-    screen.border(0)
-    screen.nodelay(1)
-    screen.keypad(0)
     screen.timeout(int(sheet.bpm_delay * 1000))  # Millisecond
 
-    screen.idcok(False)
-    screen.idlok(False)
+    # screen.immedok(True)
+    screen.idcok(True)
+    screen.idlok(True)
+
     while True:
         # screen.erase()
+        # screen.clearok(False)
         screen.clear()
         textpad.rectangle(screen, sheet_box[0][0], sheet_box[0][1], sheet_box[1][0], sheet_box[1][1])
         textpad.rectangle(screen, circuit_box[0][0], circuit_box[0][1], circuit_box[1][0], circuit_box[1][1])
-
-        curses.curs_set(0)
+        screen.border(0)
 
         for idx, line in enumerate(sheet.render()):
             left_sheet_padding = box_padding * 2 + (sheet.total_width // 2)
             top_sheet_padding = box_padding + idx
             culour.addstr(screen, top_sheet_padding, left_sheet_padding, line)
+            screen.refresh()
 
         key = screen.getch()
 
@@ -53,9 +55,9 @@ def main(screen):
 
         # culour.addstr(screen, (sh - box_padding) // 2, (sw // 3 - box_padding) // 2, str(sheet.cursor))
 
-        # screen.refresh()
         sheet.update_cursor()
         # Sleep for the remaining time if a key have been pressed ?
+        # screen.refresh()
 
 
 os.putenv("TERM", "xterm-256color")
