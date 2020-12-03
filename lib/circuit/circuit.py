fcd62aa02f9ffdb37409062aa8fc5bc8fa1af578
Pim <pim103@hotmail.fr>
2020-12-03 22:08:15 +0100

    Add predict and measure function in circuit

--- lib/circuit/circuit.py
+++ lib/circuit/circuit.py
@@ -1,6 +1,10 @@
 from lib.circuit.circuit_grid import CircuitGrid
 from lib.circuit.circuit_grid_model import CircuitGridModel
-from lib.constants import MAX_COLUMNS
+from lib.constants import MAX_COLUMNS, NUM_SHOTS
+
+from qiskit import BasicAer, execute, ClassicalRegister
+
+from copy import deepcopy
 
 
 class Circuit:
@@ -11,3 +15,31 @@ class Circuit:
 
     def render(self):
         return ""
+
+    def predict(self):
+        print("Want to predict")
+        circuit = self.circuit_grid_model.compute_circuit()
+
+        backend_sv_sim = BasicAer.get_backend('statevector_simulator')
+        job_sim = execute(circuit, backend_sv_sim, shots=NUM_SHOTS)
+        result_sim = job_sim.result()
+        quantum_state = result_sim.get_statevector(circuit, decimals=3)
+
+        for y in range(len(quantum_state)):
+            print("Value predicted for " + str(y) + " : " + str(quantum_state[y]))
+
+    def measure(self):
+        circuit = self.circuit_grid_model.compute_circuit()
+
+        backend_sv_sim = BasicAer.get_backend('qasm_simulator')
+        cr = ClassicalRegister(self.qbit_qty)
+        measure_circuit = deepcopy(circuit)  # make a copy of circuit
+        measure_circuit.add_register(cr)  # add classical registers for measurement readout
+        measure_circuit.measure(measure_circuit.qregs[0], measure_circuit.cregs[0])
+        job_sim = execute(measure_circuit, backend_sv_sim, shots=NUM_SHOTS)
+        result_sim = job_sim.result()
+        counts = result_sim.get_counts(circuit)
+
+        print(counts)
+
+        return int(list(counts.keys())[0], 2)
--- lib/circuit/input.py
+++ lib/circuit/input.py
@@ -1,3 +1,4 @@
+import curses
 from lib.constants import MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT
 
 
@@ -5,19 +6,19 @@ def handle_input(circuit, key):
     circuit_grid = circuit.circuit_grid
 
     # Left Arrow
-    if key == 75:
+    if key == curses.KEY_LEFT:
         circuit_grid.move_to_adjacent_node(MOVE_LEFT)
         circuit.render()
     # Right Arrow
-    elif key == 77:
+    elif key == curses.KEY_RIGHT:
         circuit_grid.move_to_adjacent_node(MOVE_RIGHT)
         circuit.render()
     # Up Arrow
-    elif key == 72:
+    elif key == curses.KEY_UP:
         circuit_grid.move_to_adjacent_node(MOVE_UP)
         circuit.render()
     # Down Arrow
-    elif key == 80:
+    elif key == curses.KEY_DOWN:
         circuit_grid.move_to_adjacent_node(MOVE_DOWN)
         circuit.render()
 
@@ -25,29 +26,29 @@ def handle_input(circuit, key):
     if key == ord("x"):
         circuit_grid.handle_input_x()
         circuit.render()
-        
+
     elif key == ord("y"):
         circuit_grid.handle_input_y()
         circuit.render()
-        
+
     elif key == ord("w"):
         circuit_grid.handle_input_z()
         circuit.render()
-        
+
     elif key == ord("h"):
         circuit_grid.handle_input_h()
         circuit.render()
-        
+
     # Space
     elif key == 32:
         circuit_grid.handle_input_delete()
         circuit.render()
-        
+
     elif key == ord("c"):
         # Add or remove a control
         circuit_grid.handle_input_ctrl()
         circuit.render()
-        
+
     elif key == ord("q"):
         # Move a control qubit up
         circuit_grid.handle_input_move_ctrl(MOVE_UP)
